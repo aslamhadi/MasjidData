287b3f2de4b547acf0e62cf1c0eb497dc99d1672
aslam <aslamhadi@gmail.com>
2015-02-26 19:46:37 +0700

    try scrapy

---
+++ bot/bot/items.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+# import scrapy
+from scrapy.contrib.djangoitem import DjangoItem
+from scrapy.item import Field
+
+from masjid_data.models import Masjid
+
+
+class MasjidItem(scrapy.Item):
+    django_model = Masjid
---
+++ web/masjid_data/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from masjid_data.models import Masjid
+
+admin.site.register(Masjid)
